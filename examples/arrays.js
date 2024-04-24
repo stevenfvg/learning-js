620f6ad61e2e501026e18d24585c3f16a50ae0f7
stevenfvg <stevenvalladaresgiron@gmail.com>
2024-04-24 16:13:23 -0400

    test(example): :white_check_mark: corrections are added to the examples

--- examples/arrays.js
+++ examples/arrays.js
@@ -1,13 +1,13 @@
 // Examples of arrays
 
 // Create an array
-const fruits = ['apple', 'banana', 'grape'];
+const fruits = ['Apple', 'Banana', 'Grape'];
 console.table(fruits);
 
 // Access array values
-console.log(fruits[0]); // "apple"
-console.log(fruits[1]); // "banana"
-console.log(fruits[2]); // "grape"
+console.log(fruits[0]); // "Apple"
+console.log(fruits[1]); // "Banana"
+console.log(fruits[2]); // "Grape"
 
 // Measure the length of the array
 const length = fruits.length;
@@ -28,28 +28,24 @@ fruits.forEach(fruit => {
 // Iterate and transform the 'fruits' array with .map
 console.log('Show fruits in uppercase');
 const fruitsInUppercase = fruits.map(fruit => fruit.toUpperCase());
-<<<<<<< HEAD
-
-=======
->>>>>>> examples
 console.log(fruitsInUppercase);
 
 // Add values to the beginning of the array
-fruits.unshift('raspberry');
-console.log("Array after adding 'raspberry':", fruits);
+fruits.unshift('Raspberry');
+console.log("Array after adding 'Raspberry':", fruits);
 
 // Add values to the end of the array
-fruits.push('strawberry');
-console.log("Array after adding 'strawberry':", fruits);
+fruits.push('Strawberry');
+console.log("Array after adding 'Strawberry':", fruits);
 
 // Using the Spread Operator
 let copyOfTheFruitsArray;
 // Add value to start in array copy
-copyOfTheFruitsArray = ['peach', ...fruits];
-console.log("Array after adding 'peach':", copyOfTheFruitsArray);
+copyOfTheFruitsArray = ['Peach', ...fruits];
+console.log("Array after adding 'Peach':", copyOfTheFruitsArray);
 // Add value to the end in the array copy
-copyOfTheFruitsArray = [...fruits, 'pineapple'];
-console.log("Array after adding 'pineapple':", copyOfTheFruitsArray);
+copyOfTheFruitsArray = [...fruits, 'Pineapple'];
+console.log("Array after adding 'Pineapple':", copyOfTheFruitsArray);
 
 // Delete the last value from the array
 const arrayWithTheLastElementRemoved = fruits.pop();
@@ -62,19 +58,19 @@ console.log(`First item removed: ${arrayWithFirstElementRemoved}`);
 console.log('Array after removing first element:', fruits);
 
 // Find the index of an element in the array
-const indexFound = fruits.indexOf('banana');
+const indexFound = fruits.indexOf('Banana');
 console.log(`Item: ${indexFound}`);
 
 // Delete an element at a specific position
-const deletedItem = fruits.indexOf('grape');
+const deletedItem = fruits.indexOf('Grape');
 fruits.splice(deletedItem, 1);
 console.log("Array after removing 'grape':", fruits);
 
 // Create a new array with filtered elements
-const filteredItems = fruits.filter(fruit => fruit !== 'banana');
+const filteredItems = fruits.filter(fruit => fruit !== 'Banana');
 console.log('New filtered array:', filteredItems);
 
 // Destructuring in arrays
 const [apple, banana] = fruits;
-console.log(apple); // "apple"
-console.log(banana); // "banana"
+console.log(apple); // "Apple"
+console.log(banana); // "Banana"
--- examples/variables.js
+++ examples/variables.js
@@ -2,22 +2,22 @@
 function scopeExample() {
     // Using let for a variable with function scope.
     let x = 10;
-    console.log('Initial x inside function:', x); // Output: Initial x inside function: 10
+    console.log('Initial value of x:', x); // Output: Initial x inside function: 10
 
     if (true) {
         // Using const for a variable with block scope
         const y = 20;
-        console.log('Initial y inside if block:', y); // Output: Initial y inside if block: 20
+        console.log('Value of y:', y); // Output: Initial y inside if block: 20
 
         // Reassigning let variable within the block
         x = 30;
-        console.log('Updated x inside if block:', x); // Output: Updated x inside if block: 30
+        console.log('Updated value of x:', x); // Output: Updated x inside if block: 30
 
         // Reassigning const variable within the block (will result in an error).
         // y = 40; // Error: Assignment to constant variable.
     }
 
-    console.log('Final x outside if block:', x); // Output: Final x outside if block: 30
+    console.log('Final value of x:', x); // Output: Final x outside if block: 30
 }
 
 scopeExample();
