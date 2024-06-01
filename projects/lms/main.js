30cd6f1f6ea7f2ce7d14bee2260316b1a31f9a5f
Steven Valladares <stevenvalladaresgiron@gmail.com>
2024-05-31 23:48:00 -0400

    chore(project): files for the first project are added

---
+++ projects/lms/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const app = document.querySelector('#app');
+
+    console.log('Documents uploaded successfully!');
+    console.log(app);
+});
