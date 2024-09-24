2ab37d6571cec4fbfc74942abb66b462dd682c94
Steven Valladares <stevenvalladaresgiron@gmail.com>
2024-09-24 15:54:41 -0400

    feat(challenge-1): :white_check_mark: functions are added to listen to events on the required DOM elements

--- projects/challenge-1/src/app.js
+++ projects/challenge-1/src/app.js
@@ -20,4 +20,33 @@ export function getElementsFromDom(elements) {
     console.log(productColor);
 
     // Function to load event listeners to interact with elements.
+    const loadEventListeners = () => {
+        // Adding event listener for input event on quantity input field.
+        selectedQuantity.addEventListener('input', handleInputQuantity);
+        // Adding event listener for keydown event on quantity input field.
+        selectedQuantity.addEventListener('keydown', handleKeydownQuantity);
+        // Adding event listener for click event on the calculate button.
+        btnCalculateTotal.addEventListener('click', calculateTheTotal);
+    };
+
+    // Function to handle input event for the quantity input field.
+    const handleInputQuantity = event => {
+        let quantityValue = event.target.value;
+        console.log(quantityValue);
+    };
+
+    // Function to handle keydown event for the quantity input field.
+    const handleKeydownQuantity = event => {
+        const controlKeys = ['Backspace', 'ArrowUp', 'ArrowDown'];
+        console.log(controlKeys);
+    };
+
+    // Function to calculate the total price based on quantity and product price.
+    const calculateTheTotal = event => {
+        event.preventDefault();
+        console.log('Try clicking on the calculate total button.');
+    };
+
+    // Load all the event listeners.
+    loadEventListeners();
 }
