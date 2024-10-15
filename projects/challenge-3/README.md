976243e9137b21143c212f8f9f93195fd09c061a
Steven Valladares <stevenvalladaresgiron@gmail.com>
2024-10-14 21:51:14 -0400

    chore(challenge-3): initial structure of challenge 3

---
+++ projects/challenge-3/README.md
@@ -0,0 +1,5 @@
+<div align="center">
+
+# Challenge 3 - Array and objects
+
+</div>
---
+++ projects/challenge-3/src/main.js
@@ -0,0 +1,16 @@
+import './css/styles.css';
+
+// Import sections of the layout.
+
+import { getElementsFromDom } from './app.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const app = document.querySelector('#app');
+	app.innerHTML = `
+		<div></div>
+	`;
+
+	const elements = [];
+
+	getElementsFromDom(elements);
+});
