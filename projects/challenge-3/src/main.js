29b22e2d38a7375e98d006cde3303df27af15e94
Steven Valladares <stevenvalladaresgiron@gmail.com>
2024-10-15 21:59:10 -0400

    feat(challenge-3): :heavy_plus_sign: bootstrap icons dependency is installed

--- projects/challenge-3/src/main.js
+++ projects/challenge-3/src/main.js
@@ -1,15 +1,23 @@
 // Import Bootstrap styles.
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 // Import sections of the layout.
+import filterSection from './layout/filterSection';
+import mainSection from './layout/mainSection.js';
 
 import { getElementsFromDom } from './app.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 	const app = document.querySelector('#app');
 	app.innerHTML = `
-		<div></div>
+		<div class="container-fluid vh-100">
+			<div class="row">
+				${filterSection()}
+				${mainSection()}
+			</div>
+		</div>
 	`;
 
 	const elements = [];
